f965ba6dd7404d41b6f927a2a729cfe3d86b3e7e
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2015-04-14 17:40:34 +0900

    Add ListViewSample

---
+++ ListViewSample/ListViewSample/ListViewSample/ListCS.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ListViewSample
+{
+    public class ListCS : ContentPage
+    {
+        public ListCS()
+        {
+            var list = new ListView
+            {
+                // ItemSource が List の場合は ItemTemplate に DataTemplate(typeof( が必要
+                ItemsSource = new List<Person>
+                {
+                    new Person { Name = "Taro", Age = 18 },
+                    new Person { Name = "Jiro", Age = 15 },
+                    new Person { Name = "Saburo", Age = 12 }
+                },
+                ItemTemplate = new DataTemplate(typeof(PersonCell)),
+            };
+            list.ItemTapped += List_ItemTapped;
+
+            //var list = new ListView
+            //{
+            //    // ItemSource が配列の場合は、ItemTemplate 不要
+            //    ItemsSource = new string[] { "Taro", "Jiro", "Saburo" }
+            //};
+
+            Content = list;
+        }
+
+        private void List_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Tapped: " + e.Item);
+            ((ListView)sender).SelectedItem = null; // 行選択を解除
+        }
+    }
+
+    // ViewCell の DataTemplate
+    public class PersonCell : ViewCell
+    {
+        public PersonCell()
+        {
+            // Name 表示用
+            var nameLabel = new Label { FontAttributes = FontAttributes.Bold };
+            nameLabel.SetBinding(Label.TextProperty, "Name");
+            // Age 表示用
+            var ageLabel = new Label { TextColor = Color.Navy };
+            ageLabel.SetBinding(Label.TextProperty, "Age");
+
+            View = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Padding = 5,
+                Children =
+                    {
+                        nameLabel,
+                        ageLabel
+                    },
+            };
+        }
+    }
+}
---
+++ ListViewSample/ListViewSample/ListViewSample/ListXaml.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ListViewSample
+{
+    public partial class ListXaml : ContentPage
+    {
+        public ListXaml()
+        {
+            InitializeComponent();
+
+            // BindingContext が List の場合は <ListView.ItemTemplate> が必要
+            var person = new List<Person>
+            {
+                new Person { Name = "Taro", Age = 18 },
+                new Person { Name = "Jiro", Age = 15 },
+                new Person { Name = "Saburo", Age = 12 }
+            };
+
+            // BindingContext が配列の場合は、ItemTemplate 不要
+            //var person = new string[] { "Taro", "Jiro", "Saburo" };
+
+            // 何を Binding するか (<ListView ItemsSource="{Binding person}"> だと表示できないのは要勉強…)
+            this.BindingContext = person;
+        }
+
+        private void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Tapped: " + e.Item);
+            ((ListView)sender).SelectedItem = null; // 行選択を解除
+        }
+    }
+}
