d0cbe410b039659899e628f997f0476dbf73d054
Yoshito Tabuchi <ytabuchi@xlsoft.com>
2015-04-16 16:57:31 +0900

    remake ListViewSample

---
+++ ListViewSample/ListViewSample/ListViewSample/ListXaml_CustomCell.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ListViewSample
+{
+    public partial class ListXaml_CustomCell : ContentPage
+    {
+        public ListXaml_CustomCell()
+        {
+            InitializeComponent();
+
+            const string url = "http://xamarin.com/images/index/ide-xamarin-studio.png";
+
+            var person = new List<Person>
+            {
+                new Person { Name = "Taro", Age = 18, Url = url },
+                new Person { Name = "Jiro", Age = 15, Url = url },
+                new Person { Name = "Saburo", Age = 12, Url = url },
+                new Person { Name = "Shiro", Age = 10, Url = url },
+            };
+
+            this.BindingContext = person;
+        }
+    }
+}
