fabeeb4fd8fbda745b7233a340221b08f2d5e47b
Yoshito Tabuchi <ytabuchi@xlsoft.com>
2015-06-19 15:11:44 +0900

    ViewModel経由に戻しました

--- XF_MvvmSample/XF_MvvmSample/XF_MvvmSample/S2LConverter.cs
+++ XF_MvvmSample/XF_MvvmSample/XF_MvvmSample/S2LConverter.cs
@@ -12,14 +12,16 @@ namespace XF_MvvmSample
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value == null)
+                return 0;
+
             int count = value.ToString().Length;
             return count;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            //throw new NotImplementedException();
-            return null;
+            throw new NotImplementedException();
         }
     }
 }
