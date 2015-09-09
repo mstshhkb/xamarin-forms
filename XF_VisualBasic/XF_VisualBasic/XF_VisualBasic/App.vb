2d4833e83cd63392d6e9b27121d4d808c41f50f8
Yoshito Tabuchi <ytabuchi.xlsoft@gmail.com>
2015-09-09 16:54:16 +0900

    コメント削除

--- XF_VisualBasic/XF_VisualBasic/XF_VisualBasic/App.vb
+++ XF_VisualBasic/XF_VisualBasic/XF_VisualBasic/App.vb
@@ -9,20 +9,4 @@ Public Class App
         navi.BarTextColor = Color.White
         MainPage = navi
     End Sub
-    'Public Sub New()
-    '    Dim label = New Label With {.XAlign = TextAlignment.Center,
-    '                                .FontSize = Device.GetNamedSize(NamedSize.Medium, GetType(Label)),
-    '                                .Text = "Welcome to Xamarin.Forms with Visual Basic.NET"}
-
-    '    Dim stack = New StackLayout With {
-    '        .VerticalOptions = LayoutOptions.Center
-    '    }
-    '    stack.Children.Add(label)
-
-    '    Dim page = New ContentPage
-    '    page.Content = stack
-    '    MainPage = page
-
-    'End Sub
-
 End Class
